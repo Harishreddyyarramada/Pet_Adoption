d74296cb9b904442df548e3db525d9ea6950c0c6
Harish Reddy Yarramada <129584343+Harishreddyyarramada@users.noreply.github.com>
2025-12-29 15:52:10 +0530

    Delete Client directory

--- Client/src/routes/ProtectedAdminRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function ProtectedAdminRoute() {
-  const isAdmin = localStorage.getItem("isAdmin");
-
-  return isAdmin ? <Outlet /> : <Navigate to="/admin/login" />;
-}
